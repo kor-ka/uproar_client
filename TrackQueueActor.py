7c5d384e56303f6979dcaba6250f1f3ed24a743e
gputintsev <g.putintsev@qiwi.ru>
2017-01-18 22:11:50 +0300

    add logs

--- TrackQueueActor.py
+++ TrackQueueActor.py
@@ -10,6 +10,7 @@ class Player(pykka.ThreadingActor):
 
     def play(self, track):
 
+        print ('play ' + track)
 
         sound = pyglet.media.load(track, streaming=False)
         player = sound.play()
@@ -18,6 +19,8 @@ class Player(pykka.ThreadingActor):
 
         if self.prev is not None:
             os.remove(self.prev)
+            print ('remove ' + self.prev)
+
         self.prev = track
 
     def on_receive(self, message):
@@ -34,17 +37,21 @@ class TrackQueueActor(pykka.ThreadingActor):
         if self.queue.qsize() > 0:
             self.count += 1
             track_url = self.queue.get()
+            print ('download track: ' + track_url)
             track = urllib.urlretrieve(track_url,
                                        str(self.count) + '.mp3')
             mp3_track = track[0]
+            print ('convert track to wav')
             song = AudioSegment.from_mp3(mp3_track)
             wav_track = str(self.count) + '.wav'
             song.export(wav_track, format='wav')
             os.remove(mp3_track)
+            print ('add track ' + wav_track + ' to play queue')
             self.player.tell({'command': 'play', 'track': wav_track})
 
     def on_receive(self, message):
         if message.get('command') == 'track':
+            print ('add track ' + message.get('track') + ' to download queue')
             self.queue.put(message.get('track'))
             self.actor_ref.tell({'command': 'check'})
         elif message.get('command') == 'check':
