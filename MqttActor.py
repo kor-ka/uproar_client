384d576a4c7900505e34693fcf89e15b1c49988f
kor_ka <gleb.putintsev.91@yandex.ru>
2017-01-19 10:09:50 +0000

    notify connected

--- MqttActor.py
+++ MqttActor.py
@@ -1,4 +1,4 @@
-import pykka, paho.mqtt.client as mqtt, os, urlparse
+import pykka, paho.mqtt.client as mqtt, os, urlparse, socket
 
 class MqttActor(pykka.ThreadingActor):
     client = mqtt.Client()
@@ -16,16 +16,16 @@ class MqttActor(pykka.ThreadingActor):
     # The callback for when the client receives a CONNACK response from the server.
     def on_connect(self, client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
-
-        # Subscribing in on_connect() means that if we lose the connection and
+	client.publish('server_test', socket.gethostbyname(socket.gethostname()))        # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
         client.subscribe("track_"+self.uid, 0)
 
     def initMqtt(self):
+	print('init mqtt')
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
-        url_str = os.environ.get('UPROARMQTT', 'mqtt://localhost:1883')
+        url_str = os.environ.get('UPROARMQTT', 'mqtt://eksepjal:UyPdNESZw5yo@m21.cloudmqtt.com:18552')
         url = urlparse.urlparse(url_str)
         self.client.username_pw_set(url.username, url.password)
         self.client.connect(url.hostname, url.port)
