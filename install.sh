ee022a50d4d922c4338a438dbce81366138dff7d
Gleb Putintsev <kor_ka@actor.im>
2017-01-21 20:27:03 +0300

    uncomment dependencies

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#apt-get -y install python-pip
-#apt-get -y install mpg123
-#pip install -r requirements.txt
+apt-get -y install python-pip
+apt-get -y install mpg123
+pip install -r requirements.txt
 
 cp utils/uproar /etc/init.d/uproar
 sed -i "s@uproardir@$PWD@g" "/etc/init.d/uproar"
 chmod +x /etc/init.d/uproar
+sudo update-rc.d uproar defaults 100
+
 chmod +x start_client.py
