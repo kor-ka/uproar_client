e516bd2d9fbdfb0a48d77642d3fbb8791e220d9f
Gleb Putintsev <kor_ka@actor.im>
2017-01-21 21:52:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,11 @@ apt-get -y install python-pip
 apt-get -y install mpg123
 pip install -r requirements.txt
 
+chmod +x start_client.py
+
 cp utils/uproar /etc/init.d/uproar
 sed -i "s@uproardir@$PWD@g" "/etc/init.d/uproar"
 chmod +x /etc/init.d/uproar
 sudo update-rc.d uproar defaults 100
 
-chmod +x start_client.py
+/etc/init.d/uproar start
