3ff27ae6ba4d2ed6aa4ea9f8129439d2c91bed8c
kor_ka <gleb.putintsev.91@yandex.ru>
2017-01-21 17:22:43 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#apt-get -y install python-pip
+#apt-get -y install mpg123
+#pip install -r requirements.txt
+
+cp utils/uproar /etc/init.d/uproar
+sed -i "s@uproardir@$PWD@g" "/etc/init.d/uproar"
+chmod +x /etc/init.d/uproar
+chmod +x start_client.py
