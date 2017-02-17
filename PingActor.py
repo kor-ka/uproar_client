9cb2ac88618530840fdf6fb5318275496b90e41d
kor_ka <gleb.putintsev.91@yandex.ru>
2017-02-17 13:29:14 +0000

    typos

---
+++ PingActor.py
@@ -0,0 +1,27 @@
+import urllib, time, os
+import subprocess
+from Queue import Queue
+
+import pykka
+
+
+class PingActor(pykka.ThreadingActor):
+    def __init__(self, mqtt_actor):
+        super(Player, self).__init__()
+        self.mqtt_actor = mqtt_actor
+
+    def on_start(self):
+        self.actor_ref.tell({"command":"ping"}) 
+
+    def ping(self):
+        self.mqtt_actor.tell({"command":"ping"})
+        print "--ping--"
+        time.sleep(10)
+        self.actor_ref.tell({"command":"message", "message":"ping"})
+
+    def on_receive(self, message):
+        if message.get('command') == 'ping':
+            self.ping()
+
+
+
