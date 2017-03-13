54220a57ede46c8a45cacb3608d469f82e0e0113
gputintsev <g.putintsev@qiwi.ru>
2017-03-14 02:17:24 +0300

    prevent mac from sleep

--- start_client.py
+++ start_client.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
+import sys
+
+import subprocess
+
 import MqttActor, time
 
 mqtt_ref = MqttActor.MqttActor.start()
 mqtt_ref.tell({'command': 'init'})
+
+# prevent mac from sleep
+if 'darwin' in sys.platform:
+    print('Running \'caffeinate\' on MacOSX to prevent the system from sleeping')
+    subprocess.Popen('caffeinate')
 
 # do not exit
 while True:
